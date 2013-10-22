a487854720faff7a9e4e30007f75ac59c43fcf36
j4v <xavier.garceau-aranda@polymtl.ca>
2013-10-22 10:24:17 -0400

    Added tests

---
+++ pypass/ping.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python2.7
+
+# Author                :   Xavier Garceau-Aranda
+# Date                  :   22-10-2013
+# Title                 :   httpGET.py
+# Description           :   ping go google.com
+
+#####################
+# Imports           #
+#####################
+
+import sys
+
+import scapy
+from scapy.all import *
+
+#####################
+# Main              #
+#####################
+
+
